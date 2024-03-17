eeb3b1ac6281bedb9100b154699ce00746a20a22
CoolShivas <shivas2710cool00@gmail.com>
2024-03-17 16:07:36 +0530

    Added_moves_names_selectedNum_byUseState

--- my_pokemon/src/App.jsx
+++ my_pokemon/src/App.jsx
@@ -3,15 +3,19 @@ import axios from "axios";
 
 function App() {
 
-  const [num, setNum] = useState()
+  const [num, setNum] = useState();
+  const [name, setName] = useState();
+  const [moves, setMoves] = useState();
 
   useEffect(() => {
     console.log('After re-rendering the value the useEffect will print the output.')
 
     async function getDataFromServer() {
       try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
-        console.log(response)
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/ditto/${num}`)
+        console.log(response.data.name)
+        setName(response.data.name)
+        setMoves(response.data.moves.length)
       }
       catch (error) {
         console.log(error)
@@ -21,7 +25,12 @@ function App() {
   });
 
   return <>
-    <h1> You have selected {num} </h1>
+    <h1> You have selected <span style={{ color: "red" }}> {num} </span> </h1>
+
+    <h1> My name is <span style={{ color: "red" }}> {name} </span> </h1>
+
+    <h1> I have <span style={{ color: "red" }}> {moves} moves only. </span> </h1>
+
     <select value={num} onChange={(event) => setNum(event.target.value)}>
       <option value="1"> 1 </option>
       <option value="2"> 2 </option>
