b544ad47128383f9876e9cfb0079502563e28c7d
CoolShivas <shivas2710cool00@gmail.com>
2024-03-17 15:20:45 +0530

    starting_the_project

---
+++ my_pokemon/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return <>
+    <h1> You have selected  </h1>
+    <select >
+      <option value="1"> 1 </option>
+      <option value="2"> 2 </option>
+      <option value="3"> 3 </option>
+      <option value="4"> 4 </option>
+      <option value="5"> 5 </option>
+    </select>
+  </>
+}
+
+export default App;
